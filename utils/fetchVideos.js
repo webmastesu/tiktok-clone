0871bae56feb619e28c8e90c096b0066e1c862bd
webmastesu <webmaster.tsuyoshi@gmail.com>
2024-06-25 04:17:58 -0700

    Update fetchVideos.js

--- utils/fetchVideos.js
+++ utils/fetchVideos.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchVideos = async () => {
   try {
-    const response = await axios.get('http://192.168.1.4/api.php'); // Update with your actual API endpoint
+    const response = await axios.get('https://nano.109110.xyz/api.php'); // Update with your actual API endpoint
     return response.data;
   } catch (error) {
     console.error('Error fetching videos:', error);
