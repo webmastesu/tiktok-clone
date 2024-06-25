f44695c8d15e6520cfff890d1192b8292b304496
webmastesu <webmaster.tsuyoshi@gmail.com>
2024-06-25 04:39:22 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  distDir: '.next',  // Ensure the build output directory is correctly set
 };
 
 export default nextConfig;
